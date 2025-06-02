f7fd75b8594c90f08b65b2bce094aca281c0ddb2
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2025-06-02 11:37:18 +0200

    sprawna strona zamowien

--- warehouse_manager/inventory/urls.py
+++ warehouse_manager/inventory/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 app_name = 'inventory'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('product-order/', views.product_order, name='product_order'),
     # Dodaj inne widoki jeśli są potrzebne:
     # path('products/', views.products, name='products'),
     # path('categories/', views.categories, name='categories'),
--- warehouse_manager/inventory/views.py
+++ warehouse_manager/inventory/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'inventory/index.html')
+
+def product_order(request):
+    return render(request, 'inventory/product_order.html')
