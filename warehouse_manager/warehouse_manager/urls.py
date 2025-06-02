112d0d7b7a0cc548c44fa8fa3c25f8e1a58ea052
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2025-06-02 11:03:50 +0200

    admin panel

--- warehouse_manager/warehouse_manager/urls.py
+++ warehouse_manager/warehouse_manager/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inventory.urls')),
     path('accounts/', include('allauth.urls')),
+    path('admin/', admin.site.urls),
+
 
 ]
