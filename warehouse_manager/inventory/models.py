a5b7ce8c90eaf6073436cfe98bccfce5e49d871a
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2025-06-03 10:04:46 +0200

    dodano tabele produkty i baze danych na sztywno

--- warehouse_manager/inventory/models.py
+++ warehouse_manager/inventory/models.py
@@ -1,15 +1,32 @@
 from django.db import models
 
 class Product(models.Model):
+    KSZTALT_CHOICES = [
+        ('okrągły', 'Okrągły'),
+        ('kwadratowy', 'Kwadratowy'),
+        ('niestandardowy', 'Niestandardowy'),
+    ]
+    ROZMIAR_CHOICES = [
+        ('S', 'S'),
+        ('M', 'M'),
+        ('L', 'L'),
+    ]
+    
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     quantity = models.PositiveIntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    srednica = models.DecimalField(max_digits=6, decimal_places=2)
+    ksztalt = models.CharField(max_length=20, choices=KSZTALT_CHOICES)
+    rozmiar = models.CharField(max_length=2, choices=ROZMIAR_CHOICES)
+    kolor = models.CharField(max_length=50)
+
+    
 
     def __str__(self):
-        return self.name
+        return f"{self.ksztalt} {self.rozmiar} {self.kolor} ({self.srednica} mm)"
 
 class Supplier(models.Model):
     name = models.CharField(max_length=255)
--- warehouse_manager/warehouse_manager/settings.py
+++ warehouse_manager/warehouse_manager/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'warehouse_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'cyfryzacja_db'),
+        'USER': os.getenv('DB_USER', 'posejdonkoks'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 # --------------------------------------
 # 3) Dodaj.backends, by allauth brał udział w auth
