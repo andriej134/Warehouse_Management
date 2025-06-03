d0f3a4b7571afc897de9189d2fc0185a18034f03
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2025-06-03 09:01:57 +0200

    user site

--- warehouse_manager/inventory/urls.py
+++ warehouse_manager/inventory/urls.py
@@ -7,6 +7,7 @@ app_name = 'inventory'
 urlpatterns = [
     path('', views.index, name='index'),
     path('product-order/', views.product_order, name='product_order'),
+    path('user/', views.user_profile, name='user_profile'),
     # Dodaj inne widoki jeśli są potrzebne:
     # path('products/', views.products, name='products'),
     # path('categories/', views.categories, name='categories'),
--- warehouse_manager/inventory/views.py
+++ warehouse_manager/inventory/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     return render(request, 'inventory/index.html')
 
 def product_order(request):
     return render(request, 'inventory/product_order.html')
+
+@login_required
+def user_profile(request):
+    return render(request, 'account/user_profile.html')
